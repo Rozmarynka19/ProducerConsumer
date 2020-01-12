1a948c5edca60742057997c73d01237429c82a58
Rozmarynowska Monika <rm44478>
2020-01-12 22:32:08 +0100

    Working version

--- K.c
+++ K.c
@@ -16,22 +16,26 @@ int main(int argc, char *argv[])
     {
         WaitForSingleObject(mutex,INFINITE);
 
-        //1.utwórz plik
-        int descriptor = open("warehouse.txt",O_WRONLY|O_CREAT|O_TRUNC, _S_IREAD | _S_IWRITE);
+        //1.otwórz plik
+        int descriptor = open("warehouse.txt",O_RDONLY);
         if(descriptor<0) write(2,"Creation file error",strlen("Creation file error"));
 
-        //2.odczytaj znak z stdin
-        int r = read(0,c,1);
-        if(r<0) write(2,"Read stdin error",strlen("Read stdin error"));
+        //2.odczytaj znak z pliku
+        int r = read(descriptor,c,1);
+        if(r<0) write(2,"Read file error",strlen("Read file error"));
 
-        //3.wrzuc znak do pliku
-        int w = write(descriptor,c,1);
-        if(w<0) write(2,"Write file error",strlen("Write file error"));
+        //3.wypisz na stdout
+        int w = write(1,c,1);
+        if(w<0) write(2,"Write stdout error",strlen("Write stdout error"));
 
         //4.zamknij plik
         int cl = close(descriptor);
         if(cl<0) write(2,"Close file error",strlen("Close file error"));
-        
+
+        //5.usuń plik
+        int ul = unlink("warehouse.txt");
+        if(cl<0) write(2,"Unlink file error",strlen("Unlink file error"));
+
         _Bool succeed = ReleaseMutex(mutex);
         if(succeed==0) write(2,"Mutex release failed!",strlen("Mutex release failed!"));
     }while(*c !='*');
